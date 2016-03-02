ed12f694e59808546cb63395baa4fcbd2b2a1c2a
George Sowers III <Trey@Georges-MacBook-Pro.local>
2016-03-02 10:11:03 -0600

    arithmetic update

--- arithmetic.php
+++ arithmetic.php
@@ -1,35 +1,56 @@
 <?php
 
-$a = 7;
-$b = 3;
 
-function add($a, $b)
-{
-    return $a + $b;
+function add($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a + $b;
+	} else {
+		return "ERROR: Both arguments must be numbers\n";
+	}
 }
 
-function subtract($a, $b)
-{
-    return $a - $b;
+function subtract($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a - $b;
+	} else {
+		return "ERROR: Both arguments must be numbers. \$a was {$a} \$b was {$b}\n";
+	}
 }
 
-function multiply($a, $b)
-{
-    return $a * $b;
+function multiply($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a * $b;
+    } else {
+		return "ERROR: Both arguments must be numbers. \$a was {$a} \$b was {$b}\n";
+	}
 }
 
-function divide($a, $b)
-{
-    return $a / $b;
+function divide($a, $b) {
+	$message = '';
+	if ($b == 0) {
+		return "Cant divide by zero. Input non-zero number for divisor. \$a was {$a} \$b was {$b}\n";
+    	return $a / $b;
+    } else {
+		return "ERROR: Both arguments must be numbers. \$a was {$a} \$b was {$b}\n";
+	}	
 }
 
-function modulus($a, $b)
-{
-	return $a % $b;
+function modulus($a, $b) {
+	if(is_numeric($a) && is_numeric($b)) {
+		if ($b == 0) {
+			return throwErrorMessage($a, $b, 'divisorError');	
+		}
+		return $a % $b;
+	} else {
+		return throwErrorMessage($a, $b);
+	}
 }
 
-echo add($a, $b) . PHP_EOL;
-echo subtract($a, $b) . PHP_EOL;
-echo multiply($a, $b) . PHP_EOL;
-echo divide($a, $b) . PHP_EOL;
-echo modulus($a, $b) . PHP_EOL;
+function throwErrorMessage($error = 'number', $a, $b, $error = 'number') {
+	if($error == 'numericError') {
+		return "Your inputs must be numeric. \$a was {$a} \$b was {$b}\n";
+	} else if($error == 'divisor') {
+		return "Cant divide by zero. \$a was {$a} \$b was {$b}\n";
+	}
+}
+echo modulus(10, 0) . PHP_EOL;
